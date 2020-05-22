8066864bb2363f10a13819986c5cb16f50b1cc8f
amphineko <sukareki@gmail.com>
2020-05-22 15:05:06 +0800

    client: re-design member list panel

--- Ikazuchi.Web/client/src/member_list.tsx
+++ Ikazuchi.Web/client/src/member_list.tsx
@@ -1,10 +1,10 @@
-import { faBroadcastTower, faExclamationTriangle, faPlug, faSpinner, faMicrophoneAlt, faCamera } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React, { useRef, useState, useEffect, ChangeEvent } from 'react'
+import { faBroadcastTower, faExclamationTriangle, faPlug, faSpinner, faMicrophoneAlt, faCamera, faSlash, faKey, faSatelliteDish, faSearch, faNetworkWired } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon, FontAwesomeIconProps } from '@fortawesome/react-fontawesome'
+import React, { useState, ChangeEvent } from 'react'
 import { Badge, Form } from 'react-bootstrap'
 import { v4 as uuid } from 'uuid'
 
-import { GatewayState } from './gateway/client'
+import { GatewayState, UserInfo } from './gateway/client'
 import { RtcPeerState } from './gateway/peer'
 
 export interface MemberDescriptor {
@@ -22,43 +22,84 @@ export interface MediaOptionProps {
     setRequestVideo: (value: boolean) => void
 }
 
-function MediaOptions(props: MediaOptionProps) {
-    const { activeAudio, activeVideo, requestAudio, requestVideo, setRequestAudio: setAudio, setRequestVideo: setVideo } = props
+function MediaDeviceToggle(props: {
+    active: boolean,
+    icon: FontAwesomeIconProps,
+    label: string,
+    request: boolean,
+    setRequest: (value: boolean) => void,
+}) {
+    const { active, icon, label, request, setRequest } = props
 
-    const audioCheckbox = useRef<HTMLInputElement>(null)
-    const [audioId] = useState(uuid())
-    const videoCheckbox = useRef<HTMLInputElement>(null)
-    const [videoId] = useState(uuid())
+    const [inputId] = useState(uuid())
+
+    return (
+        <Form.Check custom id={inputId} inline type="switch">
+            <Form.Check.Input
+                checked={request}
+                disabled={active !== request}
+                onChange={(event: ChangeEvent) => setRequest((event.target as HTMLInputElement).checked)}
+            />
+            <Form.Check.Label>
+                <span className={`mx-1 fa-layers fa-fw ${active ? '' : 'text-muted'}`}>
+                    {!active && <FontAwesomeIcon fixedWidth icon={faSlash} />}
+                    <FontAwesomeIcon fixedWidth {...icon} />
+                </span>
+                <span className="d-none d-xl-inline">
+                    {label}
+                </span>
+            </Form.Check.Label>
+        </Form.Check>
+    )
+}
+
+function MediaOptions(props: MediaOptionProps) {
+    const { activeAudio, activeVideo, requestAudio, requestVideo, setRequestAudio, setRequestVideo } = props
 
     return (
         <div className="media-options">
-            <div className="custom-control custom-checkbox px-0 py-1">
-                <span className={`mr-2 ${activeAudio ? '' : 'text-muted'}`}>
-                    <FontAwesomeIcon fixedWidth icon={faMicrophoneAlt} />
-                </span>
-                <Form.Check
-                    custom inline id={audioId} label="Microphone" type="switch"
-                    checked={requestAudio}
-                    disabled={activeAudio !== requestAudio}
-                    onChange={(event: ChangeEvent) => setAudio((event.target as HTMLInputElement).checked)} />
-                {!activeAudio && requestAudio && <FontAwesomeIcon fixedWidth icon={faSpinner} spin />}
-            </div>
-            <div className="custom-control custom-checkbox px-0 py-1">
-                <span className={`mr-2 ${activeVideo ? '' : 'text-muted'}`}>
-                    <FontAwesomeIcon fixedWidth icon={faCamera} />
-                </span>
-                <Form.Check
-                    custom inline id={videoId} label="Camera" type="switch"
-                    checked={requestVideo}
-                    disabled={activeVideo !== requestVideo}
-                    onChange={(event: ChangeEvent) => setVideo((event.target as HTMLInputElement).checked)} />
-                {!activeVideo && requestVideo && <FontAwesomeIcon fixedWidth icon={faSpinner} spin />}
-            </div>
+            <MediaDeviceToggle
+                active={activeAudio}
+                icon={{ icon: faMicrophoneAlt }}
+                label="Audio"
+                request={requestAudio}
+                setRequest={(value) => setRequestAudio(value)}
+            />
+            <MediaDeviceToggle
+                active={activeVideo}
+                icon={{ icon: faCamera }}
+                label="Video"
+                request={requestVideo}
+                setRequest={(value) => setRequestVideo(value)}
+            />
         </div>
     )
 }
 
-function MemberEntry(props: {
+type RtcPeerStateBadgeProps = {
+    icon: FontAwesomeIconProps,
+    text?: string,
+    variant: 'danger' | 'success' | 'warning'
+}
+
+function RtcPeerStateBadge(props: RtcPeerStateBadgeProps) {
+    const { icon, text, variant } = props
+    return (
+        <Badge pill variant={variant}>
+            <FontAwesomeIcon fixedWidth {...icon} title={text} />
+        </Badge>
+    )
+}
+
+const rtcPeerStateBadges: Record<RtcPeerState, JSX.Element> = {
+    connecting: <RtcPeerStateBadge icon={{ icon: faSearch }} text="Waiting for signaling" variant={'warning'} />,
+    disconnected: <RtcPeerStateBadge icon={{ icon: faExclamationTriangle }} text="Failed to connect" variant={'danger'} />,
+    'ice-connecting': <RtcPeerStateBadge icon={{ icon: faNetworkWired }} text="ICE connecting" variant={'warning'} />,
+    new: <RtcPeerStateBadge icon={{ icon: faExclamationTriangle }} text="New" variant={'warning'} />,
+    ready: <RtcPeerStateBadge icon={{ icon: faBroadcastTower }} text="Connected" variant={'success'} />
+}
+
+function RtcPeerEntry(props: {
     screenName: string,
     state: RtcPeerState;
 }) {
@@ -67,77 +108,47 @@ function MemberEntry(props: {
     return (
         <li className="member">
             <span className="screen-name mr-1">{screenName}</span>
-            {state === 'connecting' &&
-                <Badge pill variant="warning">
-                    <FontAwesomeIcon fixedWidth icon={faSpinner} spin />
-                </Badge>}
-            {state === 'disconnected' &&
-                <Badge pill variant="danger">
-                    <FontAwesomeIcon fixedWidth icon={faExclamationTriangle} />
-                </Badge>}
-            {state === 'new' &&
-                <Badge pill variant="info">
-                    <FontAwesomeIcon fixedWidth icon={faSpinner} spin />
-                </Badge>}
-            {state === 'ready' &&
-                <Badge pill variant="success">
-                    <FontAwesomeIcon fixedWidth icon={faBroadcastTower} />
-                </Badge>}
+            {rtcPeerStateBadges[state]}
         </li>
     )
+}
+
+type GatewayStateBadgeProps = {
+    icon: FontAwesomeIconProps,
+    text: string,
+    variant: 'danger' | 'success' | 'warning'
+}
+
+function GatewayStateBadge(props: GatewayStateBadgeProps) {
+    const { icon, text, variant } = props
+    return (
+        <Badge pill variant={variant}>
+            <span className="d-none d-xl-inline">
+                {text}
+            </span>
+            <FontAwesomeIcon fixedWidth {...icon} />
+        </Badge>
+    )
+}
+
+const gatewayStateBadges: Record<GatewayState, JSX.Element> = {
+    connected: <GatewayStateBadge icon={{ icon: faKey }} text="Authenticating" variant="warning" />,
+    connecting: <GatewayStateBadge icon={{ icon: faSpinner, spin: true }} text="Connecting" variant="warning" />,
+    disconnected: <GatewayStateBadge icon={{ icon: faExclamationTriangle }} text="Disconnected" variant="danger" />,
+    ready: <GatewayStateBadge icon={{ icon: faPlug }} text="Ready" variant="success" />
 }
 
 export function MemberList(props: {
     gatewayState: GatewayState,
     mediaOptions: MediaOptionProps,
-    peerStates: MemberDescriptor[]
+    peerInfos: Record<string, UserInfo>,
+    peerStates: Record<string, RtcPeerState>
 }) {
-    let gatewayState: Object
-    switch (props.gatewayState) {
-        case 'connected':
-            gatewayState = (
-                <span>
-                    <Badge pill variant="warning">
-                        Authenticating
-                        <FontAwesomeIcon fixedWidth icon={faSpinner} spin />
-                    </Badge>
-                </span>
-            )
-            break
-        case 'connecting':
-            gatewayState = (
-                <span>
-                    <Badge pill variant="warning">
-                        Connecting
-                        <FontAwesomeIcon fixedWidth icon={faSpinner} spin />
-                    </Badge>
-                </span>
-            )
-            break
-        case 'disconnected':
-            gatewayState = (
-                <span>
-                    <Badge variant="danger">
-                        Disconnected
-                        <FontAwesomeIcon fixedWidth icon={faExclamationTriangle} />
-                    </Badge>
-                </span>
-            )
-            break
-        case 'ready':
-            gatewayState = (
-                <span>
-                    <Badge variant="success">
-                        Ready
-                        <FontAwesomeIcon fixedWidth icon={faPlug} />
-                    </Badge>
-                </span>
-            )
-            break
-    }
+    const { gatewayState, mediaOptions, peerInfos, peerStates } = props
 
-    const peers = props.peerStates.map(peer => {
-        return (<MemberEntry screenName={peer.screenName} state={peer.state} key={peer.id} />)
+    const peers = Object.keys(peerStates).map(id => {
+        const screenName = id in peerInfos ? peerInfos[id].screenName : 'loading'
+        return (<RtcPeerEntry screenName={screenName} state={peerStates[id]} key={id} />)
     })
 
     return (
@@ -147,9 +158,11 @@ export function MemberList(props: {
             </div>
             <hr className="m-0" />
             <div className="gateway-state p-3">
-                <span className="title text-muted mr-2">Signaling</span>
+                <span className="mr-2">
+                    <FontAwesomeIcon fixedWidth icon={faSatelliteDish} />
+                </span>
                 <span className="state">
-                    {gatewayState}
+                    {gatewayStateBadges[props.gatewayState]}
                 </span>
             </div>
             <hr className="m-0" />
